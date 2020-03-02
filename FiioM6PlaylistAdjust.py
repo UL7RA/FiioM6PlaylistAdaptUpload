2e66635844ad30e58fcd9e12404ba84e267cb71f
Catalin-George Hofnar <47508362+UL7RA@users.noreply.github.com>
2020-03-02 22:51:19 +0200

    Add files via upload

---
+++ FiioM6PlaylistAdjust.py
@@ -0,0 +1,48 @@
+from ftplib import FTP
+from os import remove
+from sys import exc_info
+
+hostAddress = "192.168.1.115"
+hostPort = 2121
+username = "UL7RA"
+password = ""
+original = "Vapor.m3u8"
+processed = "VaporX.m3u8"
+directory = "Playlists"
+
+originalPlaylist = open(original,"r")
+processedPlaylist = open(processed,"w")
+
+
+for line in originalPlaylist:
+    proc = str(line)
+    proc = proc.replace("Z:\Torrents\\","/storage/external_sd/")
+    proc = proc.replace("\\","/")
+    processedPlaylist.write(proc)
+
+originalPlaylist.close()
+processedPlaylist.close()
+
+try:
+    playlistBinary = open(processed,"rb")
+    connection = FTP()
+    connection.connect(hostAddress,hostPort)
+    if username:
+        connection.login(username,password)
+        print("Logged in succesfully!")
+    if directory:
+        connection.cwd(directory)
+        print("Changed directory to %s"%(directory))
+    connection.storbinary("STOR %s" %(processed),playlistBinary,1024)
+    connection.close()
+    playlistBinary.close()
+    print("Success!")
+except:
+    exc = exc_info()[0]
+    print("Error: {}".format(exc))
+
+try:
+    remove(processed)
+except:
+    exc = exc_info()[0]
+    print("Could not delete file! Error: %s"%(exc))
